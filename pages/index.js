9aa56626713527797adaf71ffa2fe278098587e2
fabricio rios <you@example.com>
2022-05-10 12:31:05 -0300

    Configurado a requisicao inicial da API para o front. Resta ajustar a atualizacao simultanea e frontend

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,41 @@
 import TemplateDefault from '../src/templates/Default';
 import { Container } from '@material-ui/core';
-import apiEstufa from './api/apiEstufa';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import React, { Component } from 'react';
+import axios from 'axios';
 
 
 export default function Home() {
-    const [estufas, setEstufas] = useState([]);
-    const [loading, setLoading] = useState(true);
+  //Armazenar os dados das estufas
+  const [estufas, setEstufas] = useState([]);
 
+  useEffect(() => {
+    axios.get('http://localhost:4000/estufas')
+    .then((response) => {
+      setEstufas(response.data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }, []);
 
   return (
     <TemplateDefault maxWidth='sm'>
 
           <ul>
-              {estufas.map}
+            {estufas.map((estufa, key) => {
+              return (
+                <li key={key}>
+                  <h1>{estufa.nome}</h1>
+                  <p>{estufa.ordem_producao}</p>
+                </li>
+              )
+            }
+              )}
           </ul>
 
     </TemplateDefault>
   )
     }
+
+    
